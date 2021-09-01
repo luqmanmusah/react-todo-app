618cfcbcf4160903471122913691dc09203bdeb3
Luqman Musah <musahluqman@gmail.com>
2021-09-01 12:36:49 +0000

    Fix some linter errors

--- src/classBased/components/TodoItem.js
+++ src/classBased/components/TodoItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from "./TodoItem.module.css"
+import styles from './TodoItem.module.css';
 
 class TodoItem extends React.Component {
   state = {
@@ -9,61 +9,69 @@ class TodoItem extends React.Component {
   handleEditing = () => {
     this.setState({
       editing: true,
-    })
+    });
   }
 
-  handleUpdatedDone = event => {
-    if (event.key === "Enter") {
-      this.setState({ editing: false })
+  handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ editing: false });
     }
   }
 
   componentWillUnmount() {
-    console.log("Cleaning up...")
+    console.log('Cleaning up...');
   }
-  
-    render() {
-      const completedStyle = {
-        fontStyle: "italic",
-        color: "#595959",
-        opacity: 0.4,
-        textDecoration: "line-through",
-      }
-      const { completed, id, title } = this.props.todo
 
-      let viewMode = {}
-      let editMode = {}
+  render() {
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+    const { completed, id, title } = this.props.todo;
 
-        if (this.state.editing) {
-        viewMode.display = "none"
-        } else {
-        editMode.display = "none"
-}
-        return <li className={styles.item}>
-          <div onDoubleClick={this.handleEditing}>
+    const viewMode = {};
+    const editMode = {};
 
-          <input type="checkbox" className={styles.checkbox}
-            checked={completed} style={viewMode}
-            onChange={() => this.props.handleChangeProps(id)} />
-            <button 
-            onClick={() => this.props.deleteTodoProps(id)}>
-              Delete
-            </button>
-            <span style={this.props.todo.completed ? completedStyle : null}>
-            {title}
-            </span>
-          </div>
-          <input type="text" 
-          style={editMode} 
-          className={styles.textIput} 
-          value={title} 
-          onChange={e => {
-            this.props.setUpdate(e.target.value, id)
-          }}
-          onKeyDown={this.handleUpdatedDone} 
-          />
-            </li>
+    if (this.state.editing) {
+      viewMode.display = 'none';
+    } else {
+      editMode.display = 'none';
     }
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing}>
+
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            style={viewMode}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button
+            onClick={() => this.props.deleteTodoProps(id)}
+          >
+            Delete
+          </button>
+          <span style={this.props.todo.completed ? completedStyle : null}>
+            {title}
+          </span>
+        </div>
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textIput}
+          value={title}
+          onChange={(e) => {
+            this.props.setUpdate(e.target.value, id);
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
+      </li>
+    );
+  }
 }
 
 export default TodoItem;
--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,110 +1,106 @@
-import React, { useState, useEffect } from "react"
-import Header from "./Header"
-import InputTodo from "./InputTodo"
-import TodoList from "./TodoList"
-import { v4 as uuidv4 } from "uuid"
-import { Route, Switch } from "react-router-dom"
-import NotMatch from "../pages/NotMatch"
-import About from "../pages/About"
-import Navbar from "./Navbar"
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import InputTodo from './InputTodo';
+import TodoList from './TodoList';
+import NotMatch from '../pages/NotMatch';
+import About from '../pages/About';
+import Navbar from './Navbar';
 
 const TodoContainer = () => {
   // const [todos, setTodos] = useState([])
-  const [todos, setTodos] = useState(getInitialTodos())
+  const [todos, setTodos] = useState(getInitialTodos());
 
-  const handleChange = id => {
-    setTodos(prevState =>
-      prevState.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
-          }
-        }
-        return todo
-      })
-    )
-  }
+  const handleChange = (id) => {
+    setTodos((prevState) => prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    }));
+  };
 
-  const delTodo = id => {
+  const delTodo = (id) => {
     setTodos([
-      ...todos.filter(todo => {
-        return todo.id !== id
-      }),
-    ])
-  }
+      ...todos.filter((todo) => todo.id !== id),
+    ]);
+  };
 
-  const addTodoItem = title => {
+  const addTodoItem = (title) => {
     const newTodo = {
       id: uuidv4(),
-      title: title,
+      title,
       completed: false,
-    }
-    setTodos([...todos, newTodo])
-  }
+    };
+    setTodos([...todos, newTodo]);
+  };
 
   const setUpdate = (updatedTitle, id) => {
     setTodos(
-      todos.map(todo => {
+      todos.map((todo) => {
         if (todo.id === id) {
-          todo.title = updatedTitle
+          todo.title = updatedTitle;
         }
-        return todo
-      })
-    )
+        return todo;
+      }),
+    );
+  };
+
+  //   useEffect(() => {
+  //     console.log("test run")
+  //     // getting stored items
+  //     const temp = localStorage.getItem("todos")
+  //   const loadedTodos = JSON.parse(temp)
+
+  //   if (loadedTodos) {
+  //     setTodos(loadedTodos)
+  //   }
+  // }, [setTodos])
+
+  function getInitialTodos() {
+  // getting stored items
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
   }
 
-//   useEffect(() => {
-//     console.log("test run")
-//     // getting stored items
-//     const temp = localStorage.getItem("todos")
-//   const loadedTodos = JSON.parse(temp)
-
-//   if (loadedTodos) {
-//     setTodos(loadedTodos)
-//   }
-// }, [setTodos])
-
-function getInitialTodos() {
-  // getting stored items
-  const temp = localStorage.getItem("todos")
-  const savedTodos = JSON.parse(temp)
-  return savedTodos || []
-}
-
-useEffect(() => {
+  useEffect(() => {
   // storing todos items
-  const temp = JSON.stringify(todos)
-  localStorage.setItem("todos", temp)
-}, [todos])
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
 
   return (
     <>
-    <Navbar />
-    <Switch>
-    <Route exact path="/">
-    <div className="container">
-      <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={addTodoItem} />
-        <TodoList
-          todos={todos}
-          handleChangeProps={handleChange}
-          deleteTodoProps={delTodo}
-          setUpdate={setUpdate}
-        />
-      </div>
-    </div>
-    </Route>
-    <Route path="/about">
-      <About />
-    </Route>
-    <Route path="*">
-      <NotMatch />
-    </Route>
-    </Switch>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={addTodoItem} />
+              <TodoList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+              />
+            </div>
+          </div>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
     </>
-  )
-}
+  );
+};
 
-export default TodoContainer
+export default TodoContainer;
